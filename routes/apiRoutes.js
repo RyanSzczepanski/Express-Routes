6aebe2e9cfc4fefb43f4d7316c6a7f40c8d4fcbc
Ryan Szczepanski <rszczepanski@comcast.net>
2021-10-13 02:21:07 -0400

    added json return to api/notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,11 +3,11 @@ const path = require('path');
 const db = require('../db/db');
 
 router.get('/notes', (req, res) => {
-    res.send("Notes")
+    res.json(db)
 });
 
 router.post('/notes', (req, res) => {
-    res.send("Create Note");
+    res.json(req.body);
 });
 
 module.exports = router;
