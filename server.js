0927ec0cae9608d65653374b2f441d933a5e0624
Ryan Szczepanski <rszczepanski@comcast.net>
2021-10-13 03:52:31 -0400

    added enviroment variable port option

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
