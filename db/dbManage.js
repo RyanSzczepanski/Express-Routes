a37cce1cde853f2ef3f89eae1d2816913430d967
Ryan Szczepanski <rszczepanski@comcast.net>
2021-10-13 03:35:32 -0400

    added removing item and added system to update id

--- db/dbManage.js
+++ db/dbManage.js
@@ -4,12 +4,34 @@ const read = fs.readFileSync;
 const write = fs.writeFileSync;
 
 class DataBase {
+    read(){
+        return JSON.parse(read('db/db.json', 'utf8'))
+    }
+
     add(data){
         let db = [].concat(JSON.parse(read('db/db.json', 'utf8')));
+        data.id = db.length;
         db[db.length] = data;
         write('db/db.json', JSON.stringify(db));
     }
+
+    remove(id){
+        let db = [].concat(JSON.parse(read('db/db.json', 'utf8')));
+        if(id < 0 || id >= db.length){throw new Error("ID doesnt fall within array bounds!")}
+        db.splice(id, 1)
+        write('db/db.json', JSON.stringify(db));
+        updateIdsFrom(id);
+    }
 }
 
+function updateIdsFrom(start){
+    let db = [].concat(JSON.parse(read('db/db.json', 'utf8')));
+    console.log(start)
+    for(let i = start; i < db.length; i++){
+        //parse int because for some reason when i is 0 it returns i as a string rather than int
+        db[i].id = parseInt(i);
+    }
+    write('db/db.json', JSON.stringify(db))
+}
 
 module.exports = new DataBase;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,11 +4,16 @@ const db = require('../db/db');
 const DataBase = require('../db/dbManage.js');
 
 router.get('/notes', (req, res) => {
-    res.json(db)
+    res.json(DataBase.read())
 });
 
 router.post('/notes', (req, res) => {
     DataBase.add(req.body)
+    res.json(req.body);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    DataBase.remove(req.params.id)
     res.json(req.body);
 });
 
